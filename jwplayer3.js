b064dbf37579e543e1b91c078f89096f80e41694
futla99 <95879630+futla99@users.noreply.github.com>
2022-11-22 21:32:13 -0500

    Update jwplayer3.js

--- jwplayer3.js
+++ jwplayer3.js
@@ -1,4 +1,17 @@
- var key = "UecJhidW7PfNtTCKnWk4kXH0NPu7J6M0";
+ var url = "https://embed.mundiallibre.com/0232f4a440d91f15bedfd33468a6668a.js";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
+
+var key = "UecJhidW7PfNtTCKnWk4kXH0NPu7J6M0";
 
     var encrypted = u;
 
@@ -24,15 +37,3 @@
 		jwplayer().on('play', function(e) {
 		  jwplayer().setCurrentAudioTrack(lang);
 		});
-var url = "https://embed.mundiallibre.com/0232f4a440d91f15bedfd33468a6668a.js";
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
-
-xhr.send();
