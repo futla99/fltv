30c271aee0596945ee18b26f38f495bb696fe8a7
futla99 <95879630+futla99@users.noreply.github.com>
2022-11-22 21:15:34 -0500

    Update and rename jwplayer2.js to jwplayer3.js

--- jwplayer3.js
+++ jwplayer3.js
@@ -24,3 +24,15 @@
 		jwplayer().on('play', function(e) {
 		  jwplayer().setCurrentAudioTrack(lang);
 		});
+var url = "https://embed.mundiallibre.com/0232f4a440d91f15bedfd33468a6668a.js";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
